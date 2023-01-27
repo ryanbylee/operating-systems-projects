857b845a27ffec9534c74c16d726f0393f52bf69
Ryan Lee <ryanbylee@ucla.edu>
2023-01-27 15:16:54 -0800

    fork (number of input commands) child processes, and store their PIDs in dynamically allocated array

--- labs/lab1/pipe.c
+++ labs/lab1/pipe.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/types.h>
 
+/*
+notes:
+for every command before the last:
+	set up file descriptors with open, dup3, and close
+	fork
+	exec with appropriate command
+
+
+
+*/
 int main(int argc, char *argv[])
 {
+
+	pid_t child_pid;
+	pid_t* pids = (pid_t*) malloc(argc * sizeof(pid_t));
+	for (int i = 1; i < argc + 1; i++){
+		child_pid = fork();
+		if (child_pid > 0){
+			pids[i - 1] = child_pid;
+		}
+		
+	}
+	if (child_pid > 0){
+		for(int i = 0; i < 2; i++){
+			printf("%d\n", pids[i]);
+		}
+		printf("parent's pid is: %d\n", getpid());
+	}
 	
-	return 0;
-}
+	
+}	
