9eac309148b36211fb66e48f51f977ef58185efa
Ryan Lee <ryanbylee@ucla.edu>
2023-01-29 22:36:59 -0800

    implementation of pipe.c for two commands

--- labs/lab1/pipe.c
+++ labs/lab1/pipe.c
@@ -16,22 +16,42 @@ for every command before the last:
 */
 int main(int argc, char *argv[])
 {
-
-	pid_t child_pid;
+	pid_t forked_pid;
 	pid_t* pids = (pid_t*) malloc(argc * sizeof(pid_t));
-	for (int i = 1; i < argc + 1; i++){
-		child_pid = fork();
-		if (child_pid > 0){
-			pids[i - 1] = child_pid;
+	int fd[2];
+	pipe(fd);
+	for (int i = 1; i < argc; i++){
+		forked_pid = fork();
+		if (forked_pid > 0){
+			pids[i - 1] = forked_pid; 
+			close(fd[1]);
+			if (i > 1){
+				close(fd[0]);
+			}
+			
+		}else if (forked_pid < 0){
+			printf("child creation failed");
+			exit(1);
+
+		}else if (forked_pid == 0 && i == 1){
+			dup2(fd[1], STDOUT_FILENO);
+			close(fd[1]);
+			close(fd[0]);
+			if (execlp(argv[i], argv[i], NULL) == -1){
+				return -1;
+			}
+
+		}else{
+			dup2(fd[0], STDIN_FILENO);
+			close(fd[0]);
+			if (execlp(argv[i], argv[i], NULL) == -1){
+				return -1;
+			}
 		}
-		
 	}
-	if (child_pid > 0){
-		for(int i = 0; i < 2; i++){
-			printf("%d\n", pids[i]);
-		}
-		printf("parent's pid is: %d\n", getpid());
-	}
+
+
+	
 	
 	
 }	
